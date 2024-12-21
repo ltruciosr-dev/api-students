86f5d584ecf4d04eff3810b75cb0610a78701e62
Luis Trucios <ltruciosr.dev@gmail.com>
2024-12-21 16:09:50 -0300

    Improve readability

--- app.py
+++ app.py
@@ -1,96 +1,63 @@
-from flask import Flask, request, jsonify, render_template
-import json
+from flask import Flask, request, jsonify
 import sqlite3
 
 app = Flask(__name__)
 
 def db_connection():
-	conn = None
-	try:
-		conn = sqlite3.connect('students.sqlite')
-	except sqlite3.error as e:
-		print(e)
-	return conn
+    try:
+        return sqlite3.connect('students.sqlite')
+    except sqlite3.error as e:
+        print(e)
+        return None
 
-@app.route("/students" , methods=["GET","POST"])
+@app.route("/students", methods=["GET", "POST"])
 def students():
-	#access the db connection
-	conn = db_connection()
-	#access the cursor object
-	cursor = conn.cursor()
+    conn = db_connection()
+    cursor = conn.cursor()
 
-#creating our GET request for all students
-	if request.method == "GET":
-		cursor = conn.execute("SELECT * FROM students")
-		students = [
-		  dict(id = row[0], firstname = row[1], lastname = row[2], gender = row[3] , age = row[4])
-		  for row in cursor.fetchall()
-		]
+    if request.method == "GET":
+        cursor.execute("SELECT * FROM students")
+        students = [
+            dict(id=row[0], firstname=row[1], lastname=row[2], gender=row[3], age=row[4])
+            for row in cursor.fetchall()
+        ]
+        return jsonify(students) if students else jsonify([])
 
-		if students is not None:
-			return jsonify(students)
+    if request.method == "POST":
+        data = request.form
+        sql = """INSERT INTO students (firstname, lastname, gender, age) VALUES (?, ?, ?, ?)"""
+        cursor.execute(sql, (data["firstname"], data["lastname"], data["gender"], data["age"]))
+        conn.commit()
+        return f"Student with id: {cursor.lastrowid} created successfully", 201
 
-#creating our POST request for a student
-	if request.method == "POST":
-		firstname = request.form["firstname"]
-		lastname = request.form["lastname"]
-		gender = request.form["gender"]
-		age  = request.form["age"]
-		#SQL  query to INSERT a student INTO our database
-		sql = """INSERT INTO students (firstname, lastname, gender, age)
-				 VALUES (?, ?, ?, ?) """
-
-		cursor = cursor.execute(sql, (firstname, lastname, gender, age))
-		conn.commit()
-		return f"Student with id: {cursor.lastrowid} created successfully"
-
-#a route with all the neccesary request methods for a single student	
-@app.route('/student/<int:id>',methods=[ "GET", "PUT", "DELETE" ])
+@app.route('/student/<int:id>', methods=["GET", "PUT", "DELETE"])
 def student(id):
-	conn = db_connection()
-	cursor = conn.cursor()
-	student = None
+    conn = db_connection()
+    cursor = conn.cursor()
 
-#creating our GET request for a student
-	if request.method == "GET":
-		cursor.execute("SELECT * FROM students WHERE id=?",(id,) )
-		rows = cursor.fetchall()
-		for row in rows:
-			student = row
-		if student is not None:
-			return jsonify(student), 200
-		else:
-			return "Something went wrong", 404
+    if request.method == "GET":
+        cursor.execute("SELECT * FROM students WHERE id=?", (id,))
+        student = cursor.fetchone()
+        return jsonify(dict(id=student[0], firstname=student[1], lastname=student[2], gender=student[3], age=student[4])) if student else ("Student not found", 404)
 
-#creating our PUT request for a student
-	if request.method == "PUT":
-		sql = """ UPDATE students SET firstname = ?,lastname = ?, gender = ? , age = ?
-				  WHERE id = ? """
+    if request.method == "PUT":
+        data = request.form
+        sql = """UPDATE students SET firstname=?, lastname=?, gender=?, age=? WHERE id=?"""
+        cursor.execute(sql, (data["firstname"], data["lastname"], data["gender"], data["age"], id))
+        conn.commit()
+        updated_student = {
+            "id": id,
+            "firstname": data["firstname"],
+            "lastname": data["lastname"],
+            "gender": data["gender"],
+            "age": data["age"]
+        }
+        return jsonify(updated_student)
 
-		firstname = request.form["firstname"]
-		lastname = request.form["lastname"]
-		gender = request.form["gender"]
-		age = request.form["age"]
-
-		updated_student = {
-			"id": id,
-			"firstname": firstname,
-			"lastname" : lastname,
-			"gender" : gender,
-			"age" : age
-		}
-
-		conn.execute(sql,(firstname, lastname, gender, age, id))
-		conn.commit()
-		return jsonify(updated_student)
-
-#creating our DELETE request for a student
-	if request.method == "DELETE":
-		sql= """ DELETE FROM students WHERE id=? """
-		conn.execute(sql, (id,))
-		conn.commit()
-
-		return "The Student with id: {} has been deleted.".format(id),200
+    if request.method == "DELETE":
+        cursor.execute("DELETE FROM students WHERE id=?", (id,))
+        conn.commit()
+        return f"Student with id: {id} has been deleted.", 200
 
 if __name__ == '__main__':
-   app.run(host='0.0.0.0', port=8000, debug=False)
+    app.run(host='0.0.0.0', port=8000, debug=False)
